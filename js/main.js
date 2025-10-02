e8a4ed21845027ecdfc72e09d302983033b27358
Mauricio Belforte <belymau@hotmail.com>
2025-10-02 03:51:06 -0300

    Actualizacion de variables Custom Properties y agregado de overlay a la seccion desplegable

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnVerMas = document.getElementById('btn-ver-mas');
     const serviciosContainer = document.getElementById('servicios-administracion');
     const arrow = document.getElementById('arrow-indicator');
+    const overlay = document.getElementById('overlay-areas');
 
-    if (btnVerMas && serviciosContainer && arrow) {
+    if (btnVerMas && serviciosContainer && arrow && overlay) {
         btnVerMas.addEventListener('click', function () {
             const isVisible = serviciosContainer.classList.contains('servicios-visibles');
 
             if (isVisible) {
                 serviciosContainer.classList.remove('servicios-visibles');
+                overlay.classList.remove('visible');
                 btnVerMas.textContent = 'Ver más';
                 arrow.classList.remove('up'); // Quita la rotación para que apunte hacia abajo
                 arrow.classList.remove('arrow-hidden'); // Muestra la flecha
             } else {
                 serviciosContainer.classList.add('servicios-visibles');
+                overlay.classList.add('visible');
                 btnVerMas.textContent = 'Ocultar';
                 arrow.classList.add('up'); // Rota la flecha (aunque no se verá)
                 arrow.classList.add('arrow-hidden'); // Oculta la flecha
