055ffd07c00da322dae3a5519927aa4f7fead059
Mauricio Belforte <belymau@hotmail.com>
2026-01-05 19:01:19 -0300

    Pequeño cambio en el archivo scroll.js para eliminar el pequeño salto en el hover y cambio en el padding superior de la seccion de contacto

--- js/scroll.js
+++ js/scroll.js
@@ -5,7 +5,7 @@ var logo = document.getElementById("logo");
 
 
 /* --evento scroll */
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     /* --muestra la ubicacion cada vez que hagas scroll */
     //console.log(window.pageYOffset);
 
@@ -13,16 +13,16 @@ window.addEventListener("scroll", function() {
     let desplazamientoActual = window.pageYOffset;
 
     /* --condicon para ocultar o mostrar el menu */
-    if(ubicacionPrincipal >= desplazamientoActual) {
+    if (ubicacionPrincipal >= desplazamientoActual) {
         /* --si es mayor o igual se muesta */
-        
+
         logo.style.transform = "scale(1)";
         logo.style.transition = "all ease-in-out 1s";
 
         $nav.style.top = "0px";
         $nav.style.transition = "all 0.9s ease-in-out 0s"
-        
-        
+
+
         /*
         console.log('Ubicacion Principal')
         console.log(ubicacionPrincipal)
@@ -30,20 +30,20 @@ window.addEventListener("scroll", function() {
         console.log(desplazamientoActual)*/
     } else {
         /* --sino lo ocultamos añadiendo un top negativo */
-        $nav.style.top = "-89px";
+        $nav.style.top = "-88px";
         $nav.style.transition = "all 0.4s ease-in-out 0s"
 
         logo.style.transform = "scale(0.2)";
         logo.style.transition = "all ease-in-out 1s"
 
-        
 
-        
-        
-      /*  console.log('Ubicacion Principal')
-        console.log(ubicacionPrincipal)
-        console.log('desplazamiento')
-        console.log(desplazamientoActual)*/
+
+
+
+        /*  console.log('Ubicacion Principal')
+          console.log(ubicacionPrincipal)
+          console.log('desplazamiento')
+          console.log(desplazamientoActual)*/
     }
 
     /* --actulizamos la ubicacion principal */
